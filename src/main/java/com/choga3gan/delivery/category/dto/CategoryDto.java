e826f77cc82ba9616060470f2e50dd0f8b067086
jinnk0 <jin8073@gmail.com>
2025-10-31 16:50:40 +0900

    feat: category CRUD 기능 개발을 위한 인터페이스 및 DTO 생성 (#14)

--- src/main/java/com/choga3gan/delivery/category/dto/CategoryDto.java
+++ src/main/java/com/choga3gan/delivery/category/dto/CategoryDto.java
@@ -1,4 +1,27 @@
+/**
+ * @package     com.choga3gan.delivery.category.dto
+ * @class       CategoryDto
+ * @description category 생성 및 수정 요청 시 사용할 DTO
+ *
+ * @author      jinnk0
+ * @since       2025. 10. 31.
+ * @version     1.0
+ *
+ * <pre>
+ * == Modification Information ==
+ * Date          Author        Description
+ * ----------    -----------   ---------------------------
+ * 2025. 10. 31.        jinnk0       최초 생성
+ * </pre>
+ */
+
 package com.choga3gan.delivery.category.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "카테고리 생성 및 수정 요청 DTO")
 public class CategoryDto {
+
+    @Schema(description = "등록 또는 수정할 카테고리 이름 설정", example = "한식")
+    private String categoryName;
 }
--- src/main/java/com/choga3gan/delivery/category/service/CategoryService.java
+++ src/main/java/com/choga3gan/delivery/category/service/CategoryService.java
@@ -1,4 +1,40 @@
+/**
+ * @package     com.choga3gan.delivery.category.service
+ * @class       CategoryService
+ * @description 카테고리 CRUD 서비스 개발을 위한 인터페이스 구현
+ *
+ * @author      jinnk0
+ * @since       2025. 10. 31.
+ * @version     1.0
+ *
+ * <pre>
+ * == Modification Information ==
+ * Date          Author        Description
+ * ----------    -----------   ---------------------------
+ * 2025. 10. 31.        jinnk0       최초 생성
+ * </pre>
+ */
+
 package com.choga3gan.delivery.category.service;
 
-public class CategoryService {
+import com.choga3gan.delivery.category.domain.Category;
+import com.choga3gan.delivery.category.dto.CategoryDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CategoryService {
+
+    // CREATE
+    Category createCategory(CategoryDto categoryDto);
+
+    // READ
+    Category getCategory(UUID categoryId);
+    List<Category> getAllCategories();
+
+    // Update
+    Category updateCategory(CategoryDto categoryDto);
+
+    // Delete
+    void deleteCategory(UUID categoryId);
 }
