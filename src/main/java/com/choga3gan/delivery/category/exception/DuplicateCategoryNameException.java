a942425457a53fab1826c70c17ce966ed0829a04
jinnk0 <jin8073@gmail.com>
2025-11-03 16:59:52 +0900

    chore: 페이지 주석 추가 (#14)

--- src/main/java/com/choga3gan/delivery/category/exception/DuplicateCategoryNameException.java
+++ src/main/java/com/choga3gan/delivery/category/exception/DuplicateCategoryNameException.java
@@ -1,3 +1,19 @@
+/**
+ * @package     com.choga3gan.delivery.category.exception
+ * @class       DuplicateCategoryNameException
+ * @description 새로 추가하려고 하는 카테고리 이름이 이미 존재할 경우 발생하는 예외 정의
+ *
+ * @author      jinnk0
+ * @since       2025. 11. 3.
+ * @version     1.0
+ *
+ * <pre>
+ * == Modification Information ==
+ * Date          Author        Description
+ * ----------    -----------   ---------------------------
+ * 2025. 11. 3.        jinnk0       최초 생성
+ * </pre>
+ */
 package com.choga3gan.delivery.category.exception;
 
 import com.choga3gan.delivery.global.utils.exception.CustomException;
